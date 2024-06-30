c030b3694966e54e426bef7e769b058180ec5079
unknown <amv.harsh@gmail.com>
2024-07-01 00:59:28 +0530

    added the deployed backend

--- frontend/src/apiInstance/apiInstance.js
+++ frontend/src/apiInstance/apiInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://ineassignment-1.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
